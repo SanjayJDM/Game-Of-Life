67f8920216d52534503c9b2d3152bf12fda5bab0
Sanjay JDM <sanjayjdm@gmail.com>
2012-10-01 23:57:22 +0530

    Game Of Life

---
+++ GolConsoleApplication/GameOfLifeDependencies/Model/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLifeDependencies.Model
+{
+    public static class Grid
+    {
+        /// <summary>
+        /// gets/sets the number of rows in the grid
+        /// </summary>
+        public static int NumberOfRows { get; set; }
+
+        /// <summary>
+        /// gets/sets the number of cols in the grid
+        /// </summary>
+        public static int NumberOfCols { get; set; }
+
+        /// <summary>
+        /// gets/sets the number of live cels input from user
+        /// </summary>
+        public static string LiveCells {get;set;}
+
+        /// <summary>
+        /// gets/sets the number of evolution
+        /// </summary>
+        public static int NumberOfEvolution { get; set; }
+    }
+}
---
+++ GolConsoleApplication/GolConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameOfLifeDependencies;
+using GameOfLifeDependencies.Model;
+using GameOfLifeDependencies.Helpers;
+
+using GameOfLifeRuleEngine;
+
+using GameOfLifeUILibrary;
+
+namespace GolConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var UI = new GameOfLifeUILib();
+            var gameOfLife = new GameOfLife();
+            IEnumerable<GridIndex> fullGridIndex;
+           do
+            {
+                UI.ShowInputScreen();
+                fullGridIndex = gameOfLife.Initialize();
+                UI.DisplayGrid(fullGridIndex);
+
+                for (int i = 0; i <= Grid.NumberOfEvolution; i++)
+                {
+                    fullGridIndex = gameOfLife.Evolve(i, fullGridIndex.ToList<GridIndex>());
+                    UI.PrintEvolution(i);
+                    UI.DisplayGrid(fullGridIndex);
+                }
+            }
+            while (UI.Quit());
+         }
+    }
+}
